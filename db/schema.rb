79c816418193f383aef2512dd0f0821f88c3dc60
Martin Alibert <alibertmartin@mmibordeaux.com>
2023-03-02 15:35:21 +0100

    scaffold clubcard

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_01_095828) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_02_141520) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.float "chapo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "clubcards", force: :cascade do |t|
+    t.text "title"
+    t.string "name"
+    t.float "age"
+    t.text "position"
+    t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "footcards", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
