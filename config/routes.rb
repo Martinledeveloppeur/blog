a2bc193e080e836f69cbb6ae12e3bea0e2a9a6d8
Martin Alibert <alibertmartin@mmibordeaux.com>
2023-03-02 15:17:04 +0100

    scaffold clubcard

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
-Rails.application.routes.draw do
-  resources :images
-  resources :articles
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+#Rails.application.routes.draw do
+  #resources :clubcards
+  #resources :footcards
+  #resources :images
+  #resources :articles
+   #Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
+   #Defines the root path route ("/")
+   #root "articles#index"
+#end
